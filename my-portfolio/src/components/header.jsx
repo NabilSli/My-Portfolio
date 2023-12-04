9fc069df7f51de5d96184c7cb6163a4bb75bd2af
nabil Slimani <slimaninabil93350@gmail.com>
2023-12-04 10:48:43 +0100

    feat (header.jsx): add sass and create header component

---
+++ my-portfolio/src/components/header.jsx
@@ -0,0 +1,25 @@
+function Header() {
+  return (
+    <header className="header">
+      <h2 className="headerTitle">SLIMANI Nabil</h2>
+      <nav>
+        <ul className="navMenu">
+          <li>
+            <p className="navAnchor">Accueil</p>
+          </li>
+          <li>
+            <p className="navAnchor">Mes Projets</p>
+          </li>
+          <li>
+            <p className="navAnchor">Contact</p>
+          </li>
+          <li>
+            <p className="navAnchor">CV</p>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
