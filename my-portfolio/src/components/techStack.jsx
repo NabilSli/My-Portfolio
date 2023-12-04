ddc2b8c01f03add6257fce44aaf59cbdb276bf8b
nabil Slimani <slimaninabil93350@gmail.com>
2023-12-04 11:10:47 +0100

    feat (techStack.jsx): add tech stack caroussel in a component

---
+++ my-portfolio/src/components/techStack.jsx
@@ -0,0 +1,16 @@
+function TechStack() {
+  <section className="tech-stack">
+    <img src="https://img.shields.io/badge/-HTML5-000?logo=html5" alt="HTML5" />
+    <img
+      src="https://img.shields.io/badge/-JavaScript-000?logo=javascript"
+      alt="JavaScript"
+    />
+    <img
+      src="https://img.shields.io/badge/-TypeScript-000?logo=typescript"
+      alt="TypeScript"
+    />
+    <img src="https://img.shields.io/badge/-React-000?logo=react" alt="React" />
+  </section>;
+}
+
+export default TechStack;
--- my-portfolio/src/pages/Home.Page.jsx
+++ my-portfolio/src/pages/Home.Page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Description from "../components/description";
+import TechStack from "../components/techStack";
 
 export default function HomePage() {
   return (
     <section className="homeBody">
       <Description />
+      <TechStack />
     </section>
   );
 }
