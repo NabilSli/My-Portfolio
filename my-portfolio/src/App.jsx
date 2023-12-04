84a8153509d9a979fbadb640d1f711b8ee19f42d
nabil Slimani <slimaninabil93350@gmail.com>
2023-12-04 11:17:03 +0100

    feat (routes.jsx): add routes

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,32 +1,15 @@
-import "./App.css";
-
-import profilePicture from "./assets/me.png";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout";
 
 function App() {
   return (
-    <>
-      <section className="cta">
-        <button className="button-projects">Projects</button>
-      </section>
-      <section className="tech-stack">
-        <img
-          src="https://img.shields.io/badge/-HTML5-000?logo=html5"
-          alt="HTML5"
-        />
-        <img
-          src="https://img.shields.io/badge/-JavaScript-000?logo=javascript"
-          alt="JavaScript"
-        />
-        <img
-          src="https://img.shields.io/badge/-TypeScript-000?logo=typescript"
-          alt="TypeScript"
-        />
-        <img
-          src="https://img.shields.io/badge/-React-000?logo=react"
-          alt="React"
-        />
-      </section>
-    </>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
---
+++ my-portfolio/src/routes.jsx
@@ -0,0 +1,5 @@
+import Home from "./pages/Home.page";
+import Projects from "./pages/Projects";
+
+export const home = { path: "/", Element: Home };
+export const aboutUs = { path: "/projects", Element: Projects };
