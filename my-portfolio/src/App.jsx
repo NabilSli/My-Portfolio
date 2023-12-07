2bf4a8327b15d68f492b80b10446e9a2b2ea85ad
nabil Slimani <slimaninabil93350@gmail.com>
2023-12-07 20:41:16 +0100

    test

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,20 +1,23 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Layout from "./components/layout.jsx";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Layout from "./components/layout.jsx";
 
-import { home, projects, error } from "./routes.jsx";
+// import { home, projects, error } from "./routes.jsx";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Layout>
+//         <Routes>
+//           <Route path={home.path} element={<home.Element />} />
+//           <Route path={projects.path} element={<projects.Element />} />
+//           <Route path={error.path} element={<error.Element />} />
+//         </Routes>
+//       </Layout>
+//     </Router>
+//   );
+// }
 
 function App() {
-  return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path={home.path} element={<home.Element />} />
-          <Route path={projects.path} element={<projects.Element />} />
-          <Route path={error.path} element={<error.Element />} />
-        </Routes>
-      </Layout>
-    </Router>
-  );
+  return <p>hello world</p>;
 }
-
 export default App;
