b77009bdc190765b932701cd6c5783cbc8e26c5a
nabil Slimani <slimaninabil93350@gmail.com>
2023-12-09 20:51:41 +0100

    feat (gallery.jsx): create gallery component

---
+++ my-portfolio/src/components/gallery.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { Card } from "./cards";
+import useProjects from "./fetchData";
+
+// NOTE: this function uses the fetch to either show projetcs or
+//       loading while it get the data or an error if ther is no data
+export default function Gallery() {
+  const { error, isLoading, projects } = useProjects();
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    return <p>Loading ...</p>;
+  }
+
+  if (!projects || !projects.length) {
+    return <p>Aucun logements a afficher</p>;
+  }
+
+  return (
+    <section className="gallery">
+      {projects.map((project) => (
+        <Card
+          icon={project.icon}
+          title={project.title}
+          description={project.description}
+          key={project.id}
+          id={project.id}
+        />
+      ))}
+    </section>
+  );
+}
